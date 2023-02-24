a912b45f8cd2d70e72916a4ea34f591009d19bdf
Ayomikun Olalekan <ayomikunolalekan6@gmail.com>
2023-02-24 12:50:02 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
-# Python-operators
-A task designed to test a programmer knowledge on some of the fundamentals of python programming.
+## Arithmetic calculator
+Write a python program that accepts two integers from the user and performs some operations with those integers.
 
 ## Instructions
-Write a program that accepts two numbers from a user and performs some arithmetic operations with them.
+The program should greet the user with a welcome message and explain what it does, ask the user to enter an integer, and ask the user to enter another integer.
 
-The program should not accept numbers less than 0 and should print a message informing the user of that before asking the user for input again.
+It should show a list of operations for the user to choose from, ask the user which operation they want to perform, and perform the selected operation on the inputs.
 
-The program should print the sum, the difference, and the product of the two input.
+After performing the operation, the program should ask the user if they want to perform another operation. If the user says yes, the program should repeat itself by asking the user to enter an input again, if the user says no, end the program, and if the user inputs anything other than yes or no, a message should be printed and the prompt asking if the user wants to perform any other operation should be repeated until the user inputs yes or no. Pleas note that the user's yes or no should perform the required operation irrespective of it's lettercase.
 
+You should handle errors in your code by printing an error message if the user inputs a non-integer, non-listed operation number.
+
+The program should be able to perform sum, difference, product, and division operations on the integers.
 
 ## Note
 You are to write your code in the main.py.
 The finalcode.py contains the solution to the task.
-This task is designed to test your knowledge on variables, python operators, if-else statements, and loops.
-
+This task is designed to test your knowledge on variables, python operators, if-else statements, try and except, and loops.
 
 ## References
-Hackerrank.com
+Hackerrank.com, ChatGpt
--- finalcode.py
+++ finalcode.py
@@ -1,18 +1,52 @@
+print("Good day and welcome. This is an arithmetic calculator where you will be asked to supply some inputs and an operation of your choice will be performed on that input.")
+
 while True:
-  a = int(input("Enter a number: "))
-  b = int(input("Enter a number: "))
+  try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
 
-  if a < 0 or b < 0:
-    print("You can only input numbers that are greater than 0.")
-  else:
-    print(a+b)
-    print(a-b)
-    print(a*b)
-    break
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multplication")
+    print("4. Division")
+    print("Pick an operation by selecting it's corresponding number.")
+    choice = int(input("Choose an operation from the menu above: "))
 
-# Program explanation
-# First, we create a while loop and set the condition to be true.
-# Next, we collect some input from the user and store in variables a and b respectively.
-# We then set a condition to ensure that the program only performs the required operations if either of the input is greater than 0.
-# If any of the input is less than 0, we print out a message for the user, else, we perform the sum, difference and product of the two numbers.
+    if choice < 1 or choice > 4:
+        print("Please pick an operation from the list")
+    else:
+      if choice == 1:
+          result = num1 + num2
+          print(f"The sum of the two numbers is {result}")
+      elif choice == 2:
+          result = num1 - num2
+          print(f"The difference between the two numbers is {result}")
+      elif choice == 3:
+          result = num1 * num2
+          print(f"The product of the two numbers is {result}")
+      elif choice == 4:
+          result = num1/num2
+          print(f"The result of the division between the two numbers is {result}")
+      while True:
+        choice2 = input('''Would you like to perform another operation; Y for "Yes" and N for "No": ''')
+        if choice2.upper() == "Y":
+          break
+        elif choice2.upper() == "N":
+          print("Goodbye for now!")
+          exit()
+        else:
+          print("Pleas pick Y or N")          
+  except ValueError:
+     print("Please enter your input again, only numbers are accepted, decimals and letters are not accepted.")
 
+# Code Explanation
+# First, a welcome message which contains a greeting and the description of the program is displayed to the user.
+# A while loop with it's condition set to True is the created and a try statement to help avoid errors is written inside the body of the while loop.
+# A prompt asking the user for integer input is then displayed and a menu containing the list of operations that can be performed is also displayed to the user.
+# A prompt that allows the user to choose the operation they will like to perform is printed out, and conditional statements for each operation is executed.
+# For example, if the user, chooses 1 which is addition, the code under the body of the conditionl statement that controls addition is executed.
+# After the conditional statements, another while loop is created with it's condition also set to True and a prompt that asks the user if they would like to perform another operation is then printed out to the screen.
+# If the user chooses to perform another operation, that is choose "Y",the new loop is broken and the program restarts whereas if the user chooses not to continue that is "N", a message is printed out to the user, and the program ends.
+# If the user enter neither Y or N, a message is displayed to the user and the prompt asking the user to input Y or N is displayed until the user chooses to input one of them.
+# To avoid errors, an except ValueError statement is used and a message is printed out to the user if they enter an invalid input.
+#      
